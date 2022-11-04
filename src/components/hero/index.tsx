d2fac8f31cb5af3ecd57cb900586efd0cb7b7025
nairo-mudumane <nairomudumane@outlook.com>
2022-11-04 12:20:12 +0200

    fixed hero text

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -9,7 +9,7 @@ export function Hero() {
           <div className="flex flex-col items-center">
             <h2 className="text-white text-center text-3xl italic md:text-6xl lg:text-7xl font-extrabold uppercase mb-4">
               Estamos em <br />
-              manutencao
+              manutenção
             </h2>
             <MainButton text="Contacte" />
           </div>
